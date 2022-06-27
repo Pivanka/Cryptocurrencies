5d04427379a4a7ff13d794e7edf575593ec79f35
Ivanka <ivanka.polyuhovych@gmail.com>
2022-06-27 13:51:48 +0300

    Add HttpClient and load data from url

--- CryptoCurrencies/ViewModels/MainViewModel.cs
+++ CryptoCurrencies/ViewModels/MainViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CryptoCurrencies.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -12,12 +14,38 @@ namespace CryptoCurrencies.ViewModels
     {
         private readonly ObservableCollection<CoinViewModel> _coins;
         public IEnumerable<CoinViewModel> Coins => _coins;
+        public CoinList CoinResult { get; set; }
         public ICommand FindCoinCommand { get; }
 
         public MainViewModel()
         {
             _coins = new ObservableCollection<CoinViewModel>();
-            _coins.Add(new CoinViewModel(new Models.Coin("BTC", "1", "Bitcoin", "BTC", 21000)));
+            //_coins.Add(new CoinViewModel(new Models.Coin("BTC", "1", "Bitcoin", "BTC", 21000)));
+            ApiHelper.InitializeClient();
+            LoadCoins();
         }
+
+        #region Load Data
+        public async void LoadCoins()
+        {
+            string url = "https://api.coincap.io/v2/assets";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    CoinResult = await response.Content.ReadAsAsync<CoinList>();
+                    for (int i = 0; i < 10; i++)
+                    {
+                        _coins.Add(new CoinViewModel(CoinResult.Data[i]));
+                    }
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        #endregion
     }
 }
